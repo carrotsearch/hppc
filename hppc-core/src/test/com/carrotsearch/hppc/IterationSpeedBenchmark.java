90509e8ea10ddc897d7f820b0417977b7da5ea15
dawidweiss <dawidweiss@7ff1d41c-760d-0410-a7ff-a3a56f310b35>
2010-03-10 12:52:47 +0000

    Playing a bit with iterators. Looks like without store to a volatile all the iterators run at about the same speed. I suspect the CPU is basically idle waiting for memory (memory bus becomes the bottleneck).
    git-svn-id: https://carrot2.svn.sourceforge.net/svnroot/carrot2/labs/hppc/trunk@4149 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ hppc-core/src/test/com/carrotsearch/hppc/IterationSpeedBenchmark.java
@@ -0,0 +1,99 @@
+package com.carrotsearch.hppc;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.carrotsearch.hppc.cursors.*;
+import com.carrotsearch.hppc.mutables.IntHolder;
+import com.carrotsearch.hppc.procedures.*;
+import com.carrotsearch.junitbenchmarks.AbstractBenchmark;
+import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
+import com.carrotsearch.junitbenchmarks.h2.BenchmarkHistoryChart;
+import com.carrotsearch.junitbenchmarks.h2.BenchmarkMethodChart;
+
+/**
+ * Various iteration approaches on an integer list.
+ */
+@BenchmarkHistoryChart(filePrefix="CLASSNAME.history", maxRuns=50)
+@BenchmarkMethodChart(filePrefix="CLASSNAME.methods")
+@BenchmarkOptions(callgc = false, warmupRounds = 10, benchmarkRounds = 10)
+
+public class IterationSpeedBenchmark extends AbstractBenchmark
+{
+    public static final int CELLS = (1024 * 1024) * 200;
+    private static ByteArrayList list;
+    public volatile int guard; 
+
+    /* */
+    @BeforeClass
+    public static void before()
+    {
+        list = new ByteArrayList();
+        list.resize(CELLS);
+    }
+
+    /* */
+    @Test
+    public void testSimpleGetLoop() throws Exception
+    {
+        int count = 0;
+        for (int i = 0; i < list.size(); i++)
+        {
+            count += list.get(i);
+        }
+
+        this.guard = count;
+    }
+
+    /* */
+    @Test
+    public void testDirectBufferLoop() throws Exception
+    {
+        final int size = list.size();
+        final byte [] buffer = list.buffer;
+        int count = 0;
+        for (int i = 0; i < size; i++)
+        {
+            count += buffer[i];
+        }
+        this.guard = count;
+    }
+    
+    /* */
+    @Test
+    public void testIterableCursor() throws Exception
+    {
+        int count = 0;
+        for (ByteCursor c : list)
+        {
+            count += c.value;
+        }
+        this.guard = count;
+    }
+
+    /* */
+    @Test
+    public void testWithProcedureClosure()
+    {
+        final IntHolder holder = new IntHolder();
+        list.forEach(new ByteProcedure() {
+            public void apply(byte v)
+            {
+                holder.value += v;
+            }
+        });
+        this.guard = holder.value;
+    }
+
+    /* */
+    @Test
+    public void testDirectBufferWithNewFor() throws Exception
+    {
+        int count = 0;
+        for (int c : list.buffer)
+        {
+            count += c;
+        }
+        this.guard = count;
+    }
+}
