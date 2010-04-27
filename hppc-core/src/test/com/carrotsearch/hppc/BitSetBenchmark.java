4a1ac2003f8ef00b60931ea74d78f0c27ee639b2
dawidweiss <dawidweiss@7ff1d41c-760d-0410-a7ff-a3a56f310b35>
2010-04-27 10:06:11 +0000

    http://issues.carrot2.org/browse/HPPC-26
    Added different iterators to the benchmark and increased its size a bit.
    
    git-svn-id: https://carrot2.svn.sourceforge.net/svnroot/carrot2/labs/hppc/trunk@4262 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- hppc-core/src/test/com/carrotsearch/hppc/BitSetBenchmark.java
+++ hppc-core/src/test/com/carrotsearch/hppc/BitSetBenchmark.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.carrotsearch.hppc.cursors.IntCursor;
 import com.carrotsearch.junitbenchmarks.AbstractBenchmark;
 import com.carrotsearch.junitbenchmarks.h2.BenchmarkHistoryChart;
 import com.carrotsearch.junitbenchmarks.h2.BenchmarkMethodChart;
@@ -22,17 +23,22 @@ public class BitSetBenchmark extends AbstractBenchmark
     /** Pseudo-random with initial seed (repeatability). */
     private static Random rnd = new Random(0x11223344);
 
+    /** Escape analysis guard. */
+    @SuppressWarnings("unused")
+    private static volatile int guard;
+
     /* */
     @BeforeClass
     public static void before()
     {
-        final int bits = 1024 * 1024 * 128;
+        final int MB = 1024 * 1024;
+        final int bits = 128 * MB * 4;
 
         hppc = new BitSet(bits);
         jre = new java.util.BitSet(bits);
 
-        // Randomly fill every 17-th bit to speed up the test.
-        for (int i = 0; i < bits; i += 17)
+        // Randomly fill every bits (this is fairly dense distribution).
+        for (int i = 0; i < bits; i += 1 + rnd.nextInt(10))
         {
             if (rnd.nextBoolean())
             {
@@ -55,20 +61,47 @@ public class BitSetBenchmark extends AbstractBenchmark
     }
 
     @Test
+    public void testBitSetIteratorHPPC() throws Exception
+    {
+        final BitSetIterator bi = hppc.iterator();
+        int sum = 0;
+        for (int i = bi.nextSetBit(); i >= 0; i = bi.nextSetBit())
+        {
+            sum += i;
+        }
+        guard = sum;
+    }
+
+    @Test
+    public void testIntCursorIteratorHPPC() throws Exception
+    {
+        int sum = 0;
+        for (IntCursor c : hppc.asIntLookupContainer())
+        {
+            sum += c.value;
+        }
+        guard = sum;
+    }
+
+    @Test
     public void testIteratorHPPC() throws Exception
     {
+        int sum = 0;
         for (int i = hppc.nextSetBit(0); i >= 0; i = hppc.nextSetBit(i + 1))
         {
-            // do nothing.
+            sum += i;
         }
+        guard = sum;
     }
 
     @Test
     public void testIteratorJRE() throws Exception
     {
+        int sum = 0;
         for (int i = jre.nextSetBit(0); i >= 0; i = jre.nextSetBit(i + 1))
         {
-            // do nothing.
+            sum += i;
         }
+        guard = sum;
     }
 }
