80a7586b69f96b1303e344a4c6eeb73b4b15e57d
dawidweiss <dawidweiss@7ff1d41c-760d-0410-a7ff-a3a56f310b35>
2010-04-28 11:10:22 +0000

    Added oh, so-often-used final static empty arrays.
    git-svn-id: https://carrot2.svn.sourceforge.net/svnroot/carrot2/labs/hppc/trunk@4272 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ hppc-core/src/java/com/carrotsearch/hppc/EmptyArrays.java
@@ -0,0 +1,20 @@
+package com.carrotsearch.hppc;
+
+/**
+ * Empty arrays.
+ */
+public final class EmptyArrays
+{
+    public final static byte [] EMPTY_BYTE_ARRAY = new byte [0];
+    public final static short [] EMPTY_SHORT_ARRAY = new short [0];
+    public final static char [] EMPTY_CHAR_ARRAY = new char [0];
+    public final static int [] EMPTY_INT_ARRAY = new int [0];
+    public final static float [] EMPTY_FLOAT_ARRAY = new float [0];
+    public final static double [] EMPTY_DOUBLE_ARRAY = new double [0];
+    public final static Object [] EMPTY_OBJECT_ARRAY = new Object [0];
+    
+    private EmptyArrays()
+    {
+        // no instances.
+    }
+}
