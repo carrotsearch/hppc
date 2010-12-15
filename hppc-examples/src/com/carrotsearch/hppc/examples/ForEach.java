09c2f7283607b5e62bd8cd9a63aa1fb3eea765c3
dawidweiss <dawidweiss@7ff1d41c-760d-0410-a7ff-a3a56f310b35>
2010-12-15 13:35:57 +0000

    Corrected example.
    git-svn-id: https://carrot2.svn.sourceforge.net/svnroot/carrot2/labs/hppc/trunk@4656 7ff1d41c-760d-0410-a7ff-a3a56f310b35

--- hppc-examples/src/com/carrotsearch/hppc/examples/ForEach.java
+++ hppc-examples/src/com/carrotsearch/hppc/examples/ForEach.java
@@ -1,12 +1,7 @@
 package com.carrotsearch.hppc.examples;
 
-import java.util.*;
-
-import org.junit.Test;
-
 import com.carrotsearch.hppc.IntIntOpenHashMap;
-import com.carrotsearch.hppc.hash.IntMurmurHash;
-import com.carrotsearch.junitbenchmarks.AbstractBenchmark;
+import com.carrotsearch.hppc.procedures.IntIntProcedure;
 
 public class ForEach
 {
@@ -19,7 +14,8 @@ public class ForEach
         map.put(2, 5);
         map.put(3, 10);
 
-        int count = map.forEach(new IntIntProcedure() {
+        int count = map.forEach(new IntIntProcedure()
+        {
            int count;
            public void apply(int key, int value)
            {
@@ -28,55 +24,5 @@ public class ForEach
         }).count;
         System.out.println("There are " + count + " values >= 5");
         // [[[end:foreach-counting]]]
-    }
-
-    @Test
-    public void characterCountingWithJcfSmarter()
-    {
-        final char [] CHARS = DATA;
-        final Map<Integer, IntHolder> counts = new HashMap<Integer, IntHolder>();
-        for (int i = 0; i < CHARS.length - 1; i++)
-        {
-            final int bigram = CHARS[i] << 16 | CHARS[i+1];
-            final IntHolder currentCount = counts.get(bigram);
-            if (currentCount == null)
-            {
-                counts.put(bigram, new IntHolder(1));
-            }
-            else
-            {
-                currentCount.value++;
-            }
-        }
-    }
-    
-    @Test
-    public void characterCountingWithJcfNaive()
-    {
-        final char [] CHARS = DATA;
-        final Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
-        for (int i = 0; i < CHARS.length - 1; i++)
-        {
-            final int bigram = CHARS[i] << 16 | CHARS[i+1];
-            final Integer currentCount = counts.get(bigram);
-            if (currentCount == null)
-            {
-                counts.put(bigram, 1);
-            }
-            else
-            {
-                counts.put(bigram, currentCount + 1);
-            }
-        }
-    }
-
-    private static final class IntHolder
-    {
-        int value;
-
-        IntHolder(int initial)
-        {
-            value = initial;
-        }
     }
 }
