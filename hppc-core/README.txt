e5344e1d70a81ff74e71b565c7df7236327ab1ab
dawidweiss <dawidweiss@7ff1d41c-760d-0410-a7ff-a3a56f310b35>
2011-01-25 12:29:54 +0000

    Merging with POMified version.
    git-svn-id: https://carrot2.svn.sourceforge.net/svnroot/carrot2/labs/hppc/branches/0.3.x@4744 7ff1d41c-760d-0410-a7ff-a3a56f310b35

---
+++ hppc-core/README.txt
@@ -0,0 +1,60 @@
+
+HPPC, high performance primitive collections
+--------------------------------------------
+
+See JavaDoc and http://labs.carrotsearch.com/hppc.html for more info
+
+
+Development
+-----------
+
+mvn clean                Clean the working copy.
+mvn install -DskipTests  Quick compile, no tests.
+mvn -Pbenchmark          Include a benchmark round in the build.
+                         Results in: target/benchmarks
+mvn -Prelease            Release mode (javadocs, sources, zip/tgz bundles).
+                         Results in: target/*.tgz
+mvn -Prelease,sonatype   Release mode, sign and prepare upload release bundle.
+                         Results in: target/release-bundle.jar
+mvn site                 Generate reports: pmd, cpd, findbugs.
+                         Results in: target/site
+mvn -Pclover             Code coverage reports [requires -Dclover.license or global setting]
+                         Results in: target/clover/
+
+Release
+-------
+
+mvn clean deploy         Snapshot deploy to sonatype [requires proper setup in settings.xml]
+
+mvn -Prelease,sonatype clean verify 
+                         Prepare release bundle for manual staging.
+
+mvn -Psite-labs          Creates a release directory for rsyncing to labs.carrotsearch.com
+                         Results in: target/site-labs
+
+                         rsync -azv -e "ssh -p 2222" --chmod=u=rwX,g=rX,o=rX \
+                           target/site-labs/     \
+                           carrot2@hostgator.carrot2.org:./public_html/com.carrotsearch.labs/download/hppc/
+
+
+Clover
+------
+
+A local license is required for Clover support. Edit your ~/.m2/settings.xml and 
+add an active profile definiting these settings:
+
+...
+  <profiles>
+          <profile>
+            <id>clover-license</id>
+            <properties>
+              <clover.license.path>[...]\clover.license</clover.license.path>
+              <maven.clover.licenseLocation>[...]\clover.license</maven.clover.licenseLocation>
+            </properties>
+          </profile>
+  </profiles>
+
+  <activeProfiles>
+    <activeProfile>clover-license</activeProfile>
+  </activeProfiles>
+...
---
+++ hppc-core/src/test/java/com/carrotsearch/hppc/ObjectArrayListBenchmark.java
@@ -0,0 +1,117 @@
+package com.carrotsearch.hppc;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.carrotsearch.hppc.cursors.ObjectCursor;
+import com.carrotsearch.hppc.mutables.IntHolder;
+import com.carrotsearch.hppc.procedures.ObjectProcedure;
+import com.carrotsearch.junitbenchmarks.AbstractBenchmark;
+import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
+import com.carrotsearch.junitbenchmarks.annotation.*;
+
+/**
+ * Benchmark tests for {@link ObjectArrayList}.
+ */
+@BenchmarkHistoryChart(filePrefix="CLASSNAME.history", maxRuns=50)
+@BenchmarkMethodChart(filePrefix="CLASSNAME.methods")
+@BenchmarkOptions(callgc = false, warmupRounds = 5, benchmarkRounds = 10)
+/* removeIf:primitive */ 
+@SuppressWarnings("unchecked")
+/* end:removeIf */
+public class ObjectArrayListBenchmark<KType> extends AbstractBenchmark
+{
+    public static final int CELLS = (1024 * 1024) * 50;
+    private static ObjectArrayList<Object> singleton;
+    private ObjectArrayList<KType> list = (ObjectArrayList<KType>) singleton; 
+
+    private static final /* replaceIf:primitiveKType KType */ 
+        Object /* end:replaceIf */ defValue = Intrinsics.defaultKTypeValue();
+
+    /* */
+    @BeforeClass
+    public static void before()
+    {
+        singleton = new ObjectArrayList<Object>();
+        singleton.resize(CELLS);
+    }
+    
+    @AfterClass
+    public static void cleanup()
+    {
+        singleton = null;
+    }
+
+    /* */
+    @Test
+    public void testSimpleGetLoop() throws Exception
+    {
+        final ObjectArrayList<KType> list = this.list;
+        final int max = list.size();
+        int count = 0;
+        for (int i = 0; i < max; i++)
+        {
+            if (list.get(i) != defValue)
+                count++;
+        }
+        Assert.assertEquals(0, count);
+    }
+
+    /* */
+    @Test
+    public void testDirectBufferLoop() throws Exception
+    {
+        final int size = list.size();
+        final KType [] buffer = list.buffer;
+        int count = 0;
+        for (int i = 0; i < size; i++)
+        {
+            if (buffer[i] != defValue) 
+                count++;
+        }
+        Assert.assertEquals(0, count);
+    }
+
+    /* */
+    @Test
+    public void testIterableCursor() throws Exception
+    {
+        int count = 0;
+        for (ObjectCursor<KType> c : list)
+        {
+            if (c.value != defValue)
+                count++;
+        }
+        Assert.assertEquals(0, count);
+    }
+
+    /* */
+    @Test
+    public void testWithProcedureClosure()
+    {
+        final IntHolder count = new IntHolder();
+        list.forEach(new ObjectProcedure<KType>() {
+            public void apply(KType v)
+            {
+                if (v != defValue)
+                    count.value++;
+            }
+        });
+        Assert.assertEquals(0, count.value);
+    }
+
+    /* */
+    @Test
+    public void testDirectBufferWithNewFor() throws Exception
+    {
+        int count = 0;
+        for (KType c : (KType[]) list.buffer)
+        {
+            if (defValue != (KType) c)
+                count++;
+        }
+        Assert.assertEquals(0, count);
+    }
+}
