a235d85384b1a24948945f376864375ac1fa49f3
Dawid Weiss <dawid.weiss@carrot-search.com>
2011-02-27 00:02:36 +0100

    Added Caliper benchmarks in addition to JUB.

--- hppc-others/src/test/java/com/carrotsearch/hppc/BigramCounting.java
+++ hppc-others/src/test/java/com/carrotsearch/hppc/BigramCounting.java
@@ -1,22 +1,12 @@
 package com.carrotsearch.hppc;
 
-import gnu.trove.map.hash.TIntIntHashMap;
-import it.unimi.dsi.fastutil.ints.Int2IntLinkedOpenHashMap;
-import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.apache.commons.io.IOUtils;
 import org.junit.*;
 import org.junit.rules.MethodRule;
 
-import bak.pcj.map.IntKeyIntChainedHashMap;
-import bak.pcj.map.IntKeyIntOpenHashMap;
 
-import com.carrotsearch.hppc.hash.IntMurmurHash;
 import com.carrotsearch.junitbenchmarks.*;
 import com.carrotsearch.junitbenchmarks.annotation.BenchmarkHistoryChart;
 import com.carrotsearch.junitbenchmarks.annotation.BenchmarkMethodChart;
@@ -25,187 +15,16 @@ import com.carrotsearch.junitbenchmarks.h2.*;
 @BenchmarkHistoryChart(filePrefix = "CLASSNAME-history", maxRuns = 10)
 @BenchmarkMethodChart(filePrefix = "CLASSNAME-methods")
 @BenchmarkOptions(warmupRounds = 2, benchmarkRounds = 5)
-public class BigramCounting
+public class BigramCounting extends BigramCountingBase
 {
     private static H2Consumer consumer = new H2Consumer(new File(".mapadditions"));
 
     @Rule
     public MethodRule runBenchmarks = new BenchmarkRule(consumer, new WriterConsumer());
 
-    /* Prepare some test data */
-    private static char [] DATA;
-
-    /* Prevent dead code removal. */
-    @SuppressWarnings("unused")
-    private volatile int guard;
-
-    @BeforeClass
-    public static void prepareData() throws IOException
-    {
-        byte [] dta = IOUtils.toByteArray(
-            Thread.currentThread().getContextClassLoader().getResourceAsStream("books-polish.txt"));
-        DATA = new String(dta, "UTF-8").toCharArray();
-    }
-
     @AfterClass
     public static void cleanup()
     {
         consumer.close();
-    }
-
-    @Test
-    public void hppc()
-    {
-        // [[[start:bigram-counting]]]
-        // Some character data
-        final char [] CHARS = DATA;
-        
-        // We'll use a int -> int map for counting. A bigram can be encoded
-        // as an int by shifting one of the bigram's characters by 16 bits
-        // and then ORing the other character to form a 32-bit int.
-
-        /* 
-         * The input data is specific; it has low variance on lower bits and
-         * high variance overall. We need to use a better hashing function
-         * than simple identity. MurmurHash is good enough.
-         */ 
-        final IntIntOpenHashMap map = new IntIntOpenHashMap(
-            IntIntOpenHashMap.DEFAULT_CAPACITY, 
-            IntIntOpenHashMap.DEFAULT_LOAD_FACTOR, 
-            new IntMurmurHash());
-
-        for (int i = 0; i < CHARS.length - 1; i++)
-        {
-            final int bigram = CHARS[i] << 16 | CHARS[i+1];
-            map.putOrAdd(bigram, 1, 1);
-        }
-        // [[[end:bigram-counting]]]
-
-        guard = map.size();
-    }
-    
-    @Test
-    public void trove()
-    {
-        final char [] CHARS = DATA;
-        final TIntIntHashMap map = new TIntIntHashMap();
-
-        for (int i = 0; i < CHARS.length - 1; i++)
-        {
-            final int bigram = CHARS[i] << 16 | CHARS[i+1];
-            map.adjustOrPutValue(bigram, 1, 1);
-        }
-
-        guard = map.size();
-    }
-
-    @Test
-    public void fastutilOpenHashMap()
-    {
-        final char [] CHARS = DATA;
-        final Int2IntOpenHashMap map = new Int2IntOpenHashMap(); 
-        map.defaultReturnValue(0);
-
-        for (int i = 0; i < CHARS.length - 1; i++)
-        {
-            final int bigram = CHARS[i] << 16 | CHARS[i+1];
-            map.put(bigram, map.get(bigram) + 1);
-        }
-
-        guard = map.size();
-    }
-
-    @Test
-    public void fastutilLinkedOpenHashMap()
-    {
-        final char [] CHARS = DATA;
-        final Int2IntLinkedOpenHashMap map = new Int2IntLinkedOpenHashMap(); 
-        map.defaultReturnValue(0);
-
-        for (int i = 0; i < CHARS.length - 1; i++)
-        {
-            final int bigram = CHARS[i] << 16 | CHARS[i+1];
-            map.put(bigram, map.get(bigram) + 1);
-        }
-
-        guard = map.size();
-    }
-
-    @Test
-    public void pcjOpenHashMap()
-    {
-        final char [] CHARS = DATA;
-        final IntKeyIntOpenHashMap map = new IntKeyIntOpenHashMap();
-
-        for (int i = 0; i < CHARS.length - 1; i++)
-        {
-            final int bigram = CHARS[i] << 16 | CHARS[i+1];
-            map.put(bigram, map.get(bigram) + 1);
-        }
-
-        guard = map.size();
-    }
-
-    @Test
-    public void pcjChainedHashMap()
-    {
-        final char [] CHARS = DATA;
-        final IntKeyIntChainedHashMap map = new IntKeyIntChainedHashMap();
-
-        for (int i = 0; i < CHARS.length - 1; i++)
-        {
-            final int bigram = CHARS[i] << 16 | CHARS[i+1];
-            map.put(bigram, map.get(bigram) + 1);
-        }
-
-        guard = map.size();
-    }
-
-    @Test
-    public void jcf()
-    {
-        final char [] CHARS = DATA;
-        final Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < CHARS.length - 1; i++)
-        {
-            final int bigram = CHARS[i] << 16 | CHARS[i+1];
-            final Integer currentCount = map.get(bigram);
-            map.put(bigram, currentCount == null ? 1 : currentCount + 1);
-        }
-        
-        guard = map.size();
-    }
-
-    @Test
-    public void jcfWithHolder()
-    {
-        final char [] CHARS = DATA;
-        final Map<Integer, IntHolder> map = new HashMap<Integer, IntHolder>();
-        for (int i = 0; i < CHARS.length - 1; i++)
-        {
-            final int bigram = CHARS[i] << 16 | CHARS[i+1];
-            final IntHolder currentCount = map.get(bigram);
-            if (currentCount == null)
-            {
-                map.put(bigram, new IntHolder(1));
-            }
-            else
-            {
-                currentCount.value++;
-            }
-        }
-
-        guard = map.size();
-    }
-
-    /* Mutable integer. */
-    public static final class IntHolder
-    {
-        public int value;
-
-        public IntHolder(int initial)
-        {
-            value = initial;
-        }
     }
 }
---
+++ hppc-others/src/test/java/com/carrotsearch/hppc/BigramCountingCaliper.java
@@ -0,0 +1,72 @@
+package com.carrotsearch.hppc;
+
+import com.google.caliper.Param;
+import com.google.caliper.Runner;
+import com.google.caliper.SimpleBenchmark;
+
+/**
+ * Caliper version of {@link BigramCounting}.
+ */
+public class BigramCountingCaliper extends SimpleBenchmark
+{
+    private BigramCounting bc;
+
+    @Param public Library library;
+
+    public static enum Library
+    {
+        HPPC,
+        TROVE,
+        FASTUTIL_OPEN,
+        FASTUTIL_LINKED,
+        PCJ_OPEN,
+        PCJ_CHAINED,
+        JCF,
+        JCF_HOLDER
+    }
+
+    static
+    {
+        try
+        {
+            BigramCounting.prepareData();
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        bc = new BigramCounting();
+    }
+
+    public int timeLibrary(int reps)
+    {
+        for (int i = 0; i < reps; i++)
+        {
+            switch (library)
+            {
+                case HPPC: bc.hppc(); break;
+                case TROVE: bc.trove(); break;
+                case FASTUTIL_LINKED: bc.fastutilLinkedOpenHashMap(); break;
+                case FASTUTIL_OPEN: bc.fastutilOpenHashMap(); break;
+                case PCJ_CHAINED: bc.pcjChainedHashMap(); break;
+                case PCJ_OPEN: bc.pcjOpenHashMap(); break;
+                case JCF: bc.jcf(); break;
+                case JCF_HOLDER: bc.jcfWithHolder(); break;
+            }
+        }
+
+        // No need to return computation result because BigramCounting saves a guard (?).
+        return 1;
+    }
+    
+    public static void main(String [] args)
+    {
+        Runner.main(BigramCountingCaliper.class);
+    }
+}
