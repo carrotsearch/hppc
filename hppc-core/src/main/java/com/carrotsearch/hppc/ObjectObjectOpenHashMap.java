7305fdd1a61705b2aa5eaf6cf2dd27de5f9abbb6
Dawid Weiss <dawid.weiss@carrot-search.com>
2011-03-04 18:04:29 +0100

    HPPC-47: Replaced MurmurHash2 with the finalization step of MurmurHash3.

--- hppc-core/src/main/java/com/carrotsearch/hppc/ObjectObjectOpenHashMap.java
+++ hppc-core/src/main/java/com/carrotsearch/hppc/ObjectObjectOpenHashMap.java
@@ -2,6 +2,7 @@ package com.carrotsearch.hppc;
 
 import java.util.*;
 
+import com.carrotsearch.hppc.hash.MurmurHash3.*;
 import com.carrotsearch.hppc.cursors.*;
 import com.carrotsearch.hppc.hash.*;
 import com.carrotsearch.hppc.predicates.*;
