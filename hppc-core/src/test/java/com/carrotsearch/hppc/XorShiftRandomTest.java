2d3acdcb932a22c91a488067c2c19a73a5c33cee
Dawid Weiss <dawid.weiss@carrotsearch.com>
2012-11-19 09:21:41 +0100

    HPPC-72: XorShiftRandom always returns a zero. [Sergey Peretyatko] This class was used only in benchmarks and tests so unless you're using it directly you're not affected.

--- hppc-core/src/test/java/com/carrotsearch/hppc/XorShiftRandomTest.java
+++ hppc-core/src/test/java/com/carrotsearch/hppc/XorShiftRandomTest.java
@@ -22,6 +22,23 @@ public class XorShiftRandomTest
         // every bucket should get +- 1% * hits
         int limit = hits / 100;
         for (int bucketCount : buckets)
-            assertTrue(Math.abs(bucketCount - hits) <= limit);
+            assertTrue(
+                Math.abs(bucketCount - hits) + " > " + limit + "?", 
+                Math.abs(bucketCount - hits) <= limit);
     }
+    
+    @Test
+    public void testNext() {
+        XorShiftRandom random = new XorShiftRandom();
+        for (int bits = 1; bits <= 32; bits++) {
+            final long max = (1L << bits) - 1;
+            long mask = 0;
+            for (int i = 0; i < 10000; i++) {
+                final long val = ((long) random.next(bits)) & 0xffffffffL;
+                mask |= val;
+                assertTrue(val + " >= " + max + "?", val <= max);
+            }
+            assertEquals(max, mask);
+        }
+    }    
 }
