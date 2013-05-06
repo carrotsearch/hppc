4e60809dde470fef37cb2493a4f3c35d5bae4a28
Dawid Weiss <dawid.weiss@carrotsearch.com>
2013-05-06 18:33:10 +0200

    Updating dependencies.

--- hppc-benchmarks/src/main/java/com/carrotsearch/hppc/BigramCounting.java
+++ hppc-benchmarks/src/main/java/com/carrotsearch/hppc/BigramCounting.java
@@ -3,14 +3,16 @@ package com.carrotsearch.hppc;
 
 import java.io.File;
 
-import org.junit.*;
-import org.junit.rules.MethodRule;
+import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.rules.TestRule;
 
-
-import com.carrotsearch.junitbenchmarks.*;
+import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
+import com.carrotsearch.junitbenchmarks.BenchmarkRule;
+import com.carrotsearch.junitbenchmarks.WriterConsumer;
 import com.carrotsearch.junitbenchmarks.annotation.BenchmarkHistoryChart;
 import com.carrotsearch.junitbenchmarks.annotation.BenchmarkMethodChart;
-import com.carrotsearch.junitbenchmarks.h2.*;
+import com.carrotsearch.junitbenchmarks.h2.H2Consumer;
 
 @BenchmarkHistoryChart(filePrefix = "CLASSNAME-history", maxRuns = 10)
 @BenchmarkMethodChart(filePrefix = "CLASSNAME-methods")
@@ -20,7 +22,7 @@ public class BigramCounting extends BigramCountingBase
     private static H2Consumer consumer = new H2Consumer(new File(".mapadditions"));
 
     @Rule
-    public MethodRule runBenchmarks = new BenchmarkRule(consumer, new WriterConsumer());
+    public TestRule runBenchmarks = new BenchmarkRule(consumer, new WriterConsumer());
 
     @AfterClass
     public static void cleanup()
