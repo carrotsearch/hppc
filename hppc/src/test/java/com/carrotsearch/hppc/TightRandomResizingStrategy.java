608e64155453ac8bad6056b654fd6680b616245e
Dawid Weiss <dawid.weiss@carrotsearch.com>
2015-03-11 13:09:04 +0100

    Fix compilation error.

--- hppc/src/test/java/com/carrotsearch/hppc/TightRandomResizingStrategy.java
+++ hppc/src/test/java/com/carrotsearch/hppc/TightRandomResizingStrategy.java
@@ -1,5 +1,7 @@
 package com.carrotsearch.hppc;
 
+import com.carrotsearch.randomizedtesting.RandomizedContext;
+
 
 public final class TightRandomResizingStrategy implements ArraySizingStrategy {
   private final int maxRandomIncrement;
@@ -22,7 +24,11 @@ public final class TightRandomResizingStrategy implements ArraySizingStrategy {
   public int grow(int currentBufferLength, int elementsCount, int expectedAdditions) {
     growCalls++;
     // System.out.println(String.format(Locale.ROOT, "%d %d %d", currentBufferLength, elementsCount, expectedAdditions));
-    return Math.max(currentBufferLength, elementsCount + expectedAdditions) 
-        + KTypeArrayDequeTest.randomIntBetween(0, maxRandomIncrement);
+    int r = 0;
+    if (maxRandomIncrement > 0) {
+      r += RandomizedContext.current().getRandom().nextInt(maxRandomIncrement);
+    }
+
+    return Math.max(currentBufferLength, elementsCount + expectedAdditions) + r;
   }
 }
