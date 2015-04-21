731dcc7cd8507e3282b4228f67c12a8765a9c05a
Dawid Weiss <dawid.weiss@carrotsearch.com>
2015-04-21 10:57:52 +0200

    Opened up internals a bit.

--- hppc/src/main/java/com/carrotsearch/hppc/RandomizedHashOrderMixer.java
+++ hppc/src/main/java/com/carrotsearch/hppc/RandomizedHashOrderMixer.java
@@ -6,16 +6,16 @@ import java.util.concurrent.atomic.AtomicLong;
  * Randomized hash order. Does not guarantee deterministic hash ordering between
  * runs. In fact, it tries hard to avoid such guarantee.
  */
-final class RandomizedHashOrderMixer implements HashOrderMixingStrategy {
+public final class RandomizedHashOrderMixer implements HashOrderMixingStrategy {
   public final static RandomizedHashOrderMixer INSTANCE = new RandomizedHashOrderMixer();
 
   protected final AtomicLong seedMixer;
 
-  RandomizedHashOrderMixer() {
+  public RandomizedHashOrderMixer() {
     this(XorShiftRandom.next(System.nanoTime()));
   }
 
-  RandomizedHashOrderMixer(long seed) {
+  public RandomizedHashOrderMixer(long seed) {
     seedMixer = new AtomicLong(seed);
   }
 
