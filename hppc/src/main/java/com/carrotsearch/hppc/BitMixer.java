baf308b9b87faa56864401c512bab82f04aa449f
Dawid Weiss <dawid.weiss@carrotsearch.com>
2015-04-21 10:57:52 +0200

    Opened up internals a bit.

--- hppc/src/main/java/com/carrotsearch/hppc/BitMixer.java
+++ hppc/src/main/java/com/carrotsearch/hppc/BitMixer.java
@@ -8,45 +8,47 @@ package com.carrotsearch.hppc;
  */
 public final class BitMixer {
   // Don't bother mixing very small key domains much.
-  static int mix (byte key)   { return key * 0x85ebca6b; }
-  static int mix0(byte key)   { return key * 0x85ebca6b; }
+  public static int mix (byte key)   { return key * 0x85ebca6b; }
+  public static int mix0(byte key)   { return key * 0x85ebca6b; }
 
-  static int mix (short key)  { int k = key * 0x85ebca6b; return k ^= k >>> 13; }
-  static int mix0(short key)  { int k = key * 0x85ebca6b; return k ^= k >>> 13; }
-  static int mix (char  key)  { int k = key * 0x85ebca6b; return k ^= k >>> 13; }
-  static int mix0(char  key)  { int k = key * 0x85ebca6b; return k ^= k >>> 13; }
+  public static int mix (short key)  { int k = key * 0x85ebca6b; return k ^= k >>> 13; }
+  public static int mix0(short key)  { int k = key * 0x85ebca6b; return k ^= k >>> 13; }
+  public static int mix (char  key)  { int k = key * 0x85ebca6b; return k ^= k >>> 13; }
+  public static int mix0(char  key)  { int k = key * 0x85ebca6b; return k ^= k >>> 13; }
 
-  static int mix (int key)    { return murmurHash3(key); }
-  static int mix0(int key)    { return murmurHash3(key); }
+  // Do mix larger key domains.
 
-  static int mix (float key)  { return murmurHash3(Float.floatToIntBits(key)); }
-  static int mix0(float key)  { return murmurHash3(Float.floatToIntBits(key)); }
+  public static int mix (int key)    { return murmurHash3(key); }
+  public static int mix0(int key)    { return murmurHash3(key); }
 
-  static int mix (double key) { long v = Double.doubleToLongBits(key); return murmurHash3((int)((v >>> 32) ^ v)); }
-  static int mix0(double key) { long v = Double.doubleToLongBits(key); return murmurHash3((int)((v >>> 32) ^ v)); }
+  public static int mix (float key)  { return murmurHash3(Float.floatToIntBits(key)); }
+  public static int mix0(float key)  { return murmurHash3(Float.floatToIntBits(key)); }
 
-  static int mix (Object key) { return murmurHash3(key.hashCode()); }
-  static int mix0(Object key) { return key == null ? 0 : murmurHash3(key.hashCode()); }
+  public static int mix (double key) { long v = Double.doubleToLongBits(key); return murmurHash3((int)((v >>> 32) ^ v)); }
+  public static int mix0(double key) { long v = Double.doubleToLongBits(key); return murmurHash3((int)((v >>> 32) ^ v)); }
 
-  static int mix (byte key, int seed)  { return mix(key ^ seed); }
-  static int mix0(byte key, int seed)  { return mix0(key ^ seed); }
+  public static int mix (Object key) { return murmurHash3(key.hashCode()); }
+  public static int mix0(Object key) { return key == null ? 0 : murmurHash3(key.hashCode()); }
 
-  static int mix (short key, int seed) { return mix(key ^ seed); }
-  static int mix0(short key, int seed) { return mix0(key ^ seed); }
-  static int mix (char key, int seed)  { return mix(key ^ seed); }
-  static int mix0(char key, int seed)  { return mix0(key ^ seed); }
+  public static int mix (byte key, int seed)  { return mix(key ^ seed); }
+  public static int mix0(byte key, int seed)  { return mix0(key ^ seed); }
 
-  static int mix (int key, int seed)   { return mix0(key ^ seed); }
-  static int mix0(int key, int seed)   { return mix0(key ^ seed); }
+  public static int mix (short key, int seed) { return mix(key ^ seed); }
+  public static int mix0(short key, int seed) { return mix0(key ^ seed); }
+  public static int mix (char key, int seed)  { return mix(key ^ seed); }
+  public static int mix0(char key, int seed)  { return mix0(key ^ seed); }
 
-  static int mix (float key, int seed)  { return murmurHash3(Float.floatToIntBits(key) ^ seed); }
-  static int mix0(float key, int seed)  { return murmurHash3(Float.floatToIntBits(key) ^ seed); }
+  public static int mix (int key, int seed)   { return mix0(key ^ seed); }
+  public static int mix0(int key, int seed)   { return mix0(key ^ seed); }
 
-  static int mix (double key, int seed)  { long v = Double.doubleToLongBits(key); return murmurHash3((int)((v >>> 32) ^ v) ^ seed); }
-  static int mix0(double key, int seed)  { long v = Double.doubleToLongBits(key); return murmurHash3((int)((v >>> 32) ^ v) ^ seed); }
+  public static int mix (float key, int seed)  { return murmurHash3(Float.floatToIntBits(key) ^ seed); }
+  public static int mix0(float key, int seed)  { return murmurHash3(Float.floatToIntBits(key) ^ seed); }
 
-  static int mix (Object key, int seed)  { return murmurHash3(key.hashCode() ^ seed); }
-  static int mix0(Object key, int seed)  { return key == null ? 0 : murmurHash3(key.hashCode() ^ seed); }
+  public static int mix (double key, int seed)  { long v = Double.doubleToLongBits(key); return murmurHash3((int)((v >>> 32) ^ v) ^ seed); }
+  public static int mix0(double key, int seed)  { long v = Double.doubleToLongBits(key); return murmurHash3((int)((v >>> 32) ^ v) ^ seed); }
+
+  public static int mix (Object key, int seed)  { return murmurHash3(key.hashCode() ^ seed); }
+  public static int mix0(Object key, int seed)  { return key == null ? 0 : murmurHash3(key.hashCode() ^ seed); }
 
   /** */
   private static int murmurHash3(int k) {
