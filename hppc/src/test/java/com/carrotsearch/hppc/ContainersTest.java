9bf30c8b169bc2c284d4fa20fd9b17d0c61cbf84
Dawid Weiss <dawid.weiss@carrotsearch.com>
2015-05-04 13:47:14 +0200

    HPPC-149: added test. improved initial seed for very short time spans.

---
+++ hppc/src/test/java/com/carrotsearch/hppc/ContainersTest.java
@@ -0,0 +1,38 @@
+package com.carrotsearch.hppc;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+
+import com.carrotsearch.randomizedtesting.RandomizedTest;
+import com.carrotsearch.randomizedtesting.rules.SystemPropertiesRestoreRule;
+
+/* */
+public class ContainersTest extends RandomizedTest {
+  @Rule
+  public final TestRule rules = RuleChain.outerRule(
+      new SystemPropertiesRestoreRule());
+  
+  @After
+  public void resetState() {
+    Containers.test$reset();
+  }
+
+  @Test
+  public void testNoTestsSeed() {
+    Assertions.assertThat(System.getProperty("tests.seed")).isNull();
+    Assertions.assertThat(Containers.randomSeed64())
+              .isNotEqualTo(Containers.randomSeed64());
+  }
+  
+  @Test
+  public void testWithTestsSeed() {
+    System.setProperty("tests.seed", "deadbeef");
+    Containers.test$reset();
+    Assertions.assertThat(Containers.randomSeed64())
+              .isEqualTo(Containers.randomSeed64());
+  }
+}
