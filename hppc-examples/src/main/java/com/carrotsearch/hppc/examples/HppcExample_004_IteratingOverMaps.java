3ca4f3f58f88a3310765ad9f24de51829ec96572
Dawid Weiss <dawid.weiss@carrotsearch.com>
2015-05-06 13:14:48 +0200

    Adding API examples ported from the previous version.

---
+++ hppc-examples/src/main/java/com/carrotsearch/hppc/examples/HppcExample_004_IteratingOverMaps.java
@@ -0,0 +1,72 @@
+package com.carrotsearch.hppc.examples;
+
+import org.junit.Test;
+
+import com.carrotsearch.hppc.IntCharHashMap;
+import com.carrotsearch.hppc.cursors.IntCharCursor;
+import com.carrotsearch.hppc.procedures.IntCharProcedure;
+
+public class HppcExample_004_IteratingOverMaps
+{
+  IntCharHashMap prepare(int size)
+    {
+        final IntCharHashMap map = new IntCharHashMap(size);
+        for (int i = 0; i < size / 2; i++)
+        {
+            map.put(i, (char)(64 + i));
+        }
+        return map;
+    }
+    
+    @Test
+    public void testIterableCursor()
+    {
+        // [[[start:iteration-maps-using-iterator]]]
+        // Prepare some set to iterate over
+        final IntCharHashMap map = prepare(10);
+        
+        // Maps implement the Iterable interface that returns [keyType][valueType]Cursors 
+        // The cursor contains the key, value and internal index of the current element. 
+        for (IntCharCursor c : map)
+        {
+            System.out.println(c.index + ": " + c.key + " -> " + c.value);
+        }
+        // [[[end:iteration-maps-using-iterator]]]
+    }
+    
+    @Test
+    public void testWithProcedureClosure()
+    {
+        // [[[start:iteration-maps-using-procedures]]]
+        final IntCharHashMap map = prepare(10);
+
+        // Maps also support iteration through [keyType][valueType]Procedure interfaces.
+        // The apply() method will be called once for each key/value pair in the map.
+        
+        // Iteration from head to tail
+        map.forEach(new IntCharProcedure()
+        {
+            public void apply(int key, char value)
+            {
+                System.out.println(key + " -> " + value);
+            }
+        });
+        // [[[end:iteration-maps-using-procedures]]]
+    }
+
+    @Test
+    public void testDirectBufferLoop() throws Exception
+    {
+        // [[[start:iteration-maps-using-direct-buffer-access]]]
+        final IntCharHashMap map = prepare(10);
+        
+        // For the fastest iteration, you can access the sets's data buffers directly.
+        final int [] keys = map.keys;
+        final char [] values = map.values;
+        
+        // Note that the loop is bounded by states.length, not keys.length. This
+        // can make the code faster due to range check elimination
+        // (http://wikis.sun.com/display/HotSpotInternals/RangeCheckElimination).
+        // [[[end:iteration-maps-using-direct-buffer-access]]]
+    }
+}
