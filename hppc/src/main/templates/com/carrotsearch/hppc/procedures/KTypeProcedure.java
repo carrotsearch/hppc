bfe798ee25149f4168d8d4e57ab207aac1e4983b
Erich Schubert <kno10@users.noreply.github.com>
2022-01-30 15:50:55 +0100

    Make KTypeProcedure a @FunctionalInterface
    To allow using lambdas.

--- hppc/src/main/templates/com/carrotsearch/hppc/procedures/KTypeProcedure.java
+++ hppc/src/main/templates/com/carrotsearch/hppc/procedures/KTypeProcedure.java
@@ -4,6 +4,7 @@ package com.carrotsearch.hppc.procedures;
  * A procedure that applies to <code>KType</code> objects.
  */
 /*! ${TemplateOptions.generatedAnnotation} !*/
+@FunctionalInterface
 public interface KTypeProcedure<KType> {
   public void apply(KType value);
 }
