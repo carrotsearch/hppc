debb0b1eebab44eaa55c663f79c138e876274fb5
Erich Schubert <kno10@users.noreply.github.com>
2022-01-30 15:54:14 +0100

    Add @FunctionalInterface to KTypeComparator

--- hppc/src/main/templates/com/carrotsearch/hppc/comparators/KTypeComparator.java
+++ hppc/src/main/templates/com/carrotsearch/hppc/comparators/KTypeComparator.java
@@ -5,6 +5,7 @@ package com.carrotsearch.hppc.comparators;
  * Compares two <code>KType</code> values.
  */
 /*! ${TemplateOptions.generatedAnnotation} !*/
+@FunctionalInterface
 public interface KTypeComparator<KType> {
   int compare(KType a, KType b);
 
