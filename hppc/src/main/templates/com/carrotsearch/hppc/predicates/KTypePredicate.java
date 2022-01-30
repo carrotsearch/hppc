4aa47996f376260b28577653af5c08bb075aad89
Erich Schubert <kno10@users.noreply.github.com>
2022-01-30 15:53:11 +0100

    Add @FunctionalInterface to KTypePredicate

--- hppc/src/main/templates/com/carrotsearch/hppc/predicates/KTypePredicate.java
+++ hppc/src/main/templates/com/carrotsearch/hppc/predicates/KTypePredicate.java
@@ -4,6 +4,7 @@ package com.carrotsearch.hppc.predicates;
  * A predicate that applies to <code>KType</code> objects.
  */
 /*! ${TemplateOptions.generatedAnnotation} !*/
+@FunctionalInterface
 public interface KTypePredicate<KType> {
   public boolean apply(KType value);
 }
