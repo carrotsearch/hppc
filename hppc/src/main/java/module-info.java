d569a8944091844c62349646f8eeaf35ebfb5ba6
Dawid Weiss <dawid.weiss@carrotsearch.com>
2024-05-22 10:31:57 +0200

    Make hppc a proper Java module. The tests still run in classpath mode. #243

---
+++ hppc/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.carrotsearch.hppc {
+  requires java.logging;
+
+  exports com.carrotsearch.hppc;
+  exports com.carrotsearch.hppc.cursors;
+  exports com.carrotsearch.hppc.procedures;
+  exports com.carrotsearch.hppc.comparators;
+  exports com.carrotsearch.hppc.predicates;
+  exports com.carrotsearch.hppc.sorting;
+}
